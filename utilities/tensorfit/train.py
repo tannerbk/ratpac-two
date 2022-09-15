1ba20f697f490b79f9de26fbb925059b5af865be
Morgan Askins <maskins@ucdavis.edu>
2022-09-15 12:39:46 -0700

    tensors

--- utilities/tensorfit/train.py
+++ utilities/tensorfit/train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import uproot
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -14,8 +15,9 @@ def DirectionLossFunction(y_true, y_pred):
 class eostf:
     xdim = 26
     ydim = 26
-    rail = 10.0
-    tmod = 10.0
+    rail = 100.0
+    tmod = 1/ 200.0
+    tshift = 50
     split = 0.8
     def __init__(self, trainingFile, **kwargs):
         self.training_file = trainingFile
@@ -29,6 +31,12 @@ class eostf:
         self.labels = ['mcx', 'mcy', 'mcz'] if self.module == "position" else ['mcu', 'mcv', 'mcw']
         self.dims = len(self.labels)
         self.modelShape = (self.xdim, self.ydim, self.dims)
+
+        tfile = uproot.open(trainingFile)
+        meta = tfile['meta'].arrays(library="np")
+        self.gen_size = int(meta['dsentries'][0] / self.batchsize)
+        del tfile
+
         self.define_model()
 
     def generateImages(self, data, meta):
@@ -43,9 +51,10 @@ class eostf:
             phi = np.floor((np.arctan2(yy,xx)+np.pi-1e-9)/2/np.pi*self.ydim).astype(int)
             red = np.zeros((self.xdim,self.ydim))
             green = np.zeros((self.xdim,self.ydim))
-            red[theta,phi] = htime*self.tmod/255
+            red[theta,phi] = (htime + self.tshift)*self.tmod
             green[theta,phi] = event/self.rail
             images.append(np.dstack([red,green]))
+            #images.append(np.dstack([red]))
         return np.array(images)
 
     def prepData(self, df):
@@ -56,7 +65,6 @@ class eostf:
     def load_data(self, tfileName, index):
         tfile = uproot.open(tfileName)
         meta = tfile['meta'].arrays(library="np")
-        self.batchgen_size = int(meta['dsentries'][0] / self.batchsize)
         data = tfile['output'].arrays(
                 ['mcx','mcy','mcz','mcu','mcv','mcw','hitPMTTime','hitPMTCharge','hitPMTID'],
                 library="np",
@@ -71,7 +79,6 @@ class eostf:
     def load_validation(self, tfileName, index):
         tfile = uproot.open(tfileName)
         meta = tfile['meta'].arrays(library="np")
-        self.validgen_size = int(meta['dsentries'][0] / self.batchsize)
         data = tfile['output'].arrays(
                 ['mcx','mcy','mcz','mcu','mcv','mcw','hitPMTTime','hitPMTCharge','hitPMTID'],
                 library="np",
@@ -84,21 +91,19 @@ class eostf:
         return imageSet, dataLabel
 
     def batch_generator(self, tfileName):
-        self.batchgen_size = 1
         index = 1
         while True:
             yield self.load_data(tfileName, index-1)
-            if index < self.batchgen_size:
+            if index < self.gen_size:
                 index += 1
             else:
                 index = 1
 
     def validation_generator(self, tfileName):
-        self.validgen_size = 1
         index = 1
         while True:
             yield self.load_validation(tfileName, index-1)
-            if index < self.validgen_size:
+            if index < self.gen_size:
                 index += 1
             else:
                 index = 1
@@ -106,7 +111,7 @@ class eostf:
     def define_model(self):
         self.model = keras.models.Sequential()
 
-        dropLevel = 0.0
+        dropLevel = 0.25
         self.model.add(layers.Conv2D(16, (3,3), activation='relu', input_shape=(self.xdim,self.ydim,2) ))
         self.model.add(layers.BatchNormalization())
         self.model.add(layers.Conv2D(32, (3,3), activation='relu' ))
@@ -137,14 +142,40 @@ class eostf:
 
         self.model.compile(optimizer=self.optimizer, loss=self.loss, metrics=[self.loss])
         self.histories = []
-        for i in range(self.iterations):
-            training_set, training_labels = next(datagen)
-            validation_set, validation_labels = next(valigen)
-            history = self.model.fit( training_set, training_labels, epochs=10, verbose=0,
-                                 validation_data=(validation_set, validation_labels))
-            self.histories.append(history)
-            print(f"Done with: {i}/{self.iterations}", end="\r")
+        # Start with a quick blast through a single batch, then refine
+        training_set, training_labels = next(datagen)
+        validation_set, validation_labels = next(valigen)
+        history = self.model.fit( training_set, training_labels, epochs=40, verbose=2,
+                             validation_data=(validation_set, validation_labels))
+        self.histories.append(history)
+
+        del training_set, training_labels, validation_set, validation_labels
+        #for i in range(self.iterations):
+        #    training_set, training_labels = next(datagen)
+        #    validation_set, validation_labels = next(valigen)
+        #    history = self.model.fit( training_set, training_labels, epochs=10, verbose=0,
+        #                         validation_data=(validation_set, validation_labels))
+        #    self.histories.append(history)
+        #    print(f"Done with: {i}/{self.iterations}", end="\r")
+        history = self.model.fit( datagen, epochs=self.iterations, validation_data=valigen, verbose=2,
+                steps_per_epoch = self.gen_size, validation_steps = self.gen_size
+                )
+        self.histories.append(history)
         print(f"\n** Complete **")
+
+    def display_results(self):
+        z = np.concatenate([h.history['mse'] for h in self.histories])
+        plt.plot(z)
+        plt.show()
+        if self.module == "position":
+            testset, testres = next( self.validation_generator(self.training_file) )
+            x = self.model.predict(testset)
+            z = x.T[0] - testres.T[0]
+            mn = np.mean(z)
+            sd = np.std(z)
+            plt.hist(z, bins=100, label=f"Mean: {mn:0.1f}\nStd: {sd:0.1f}")
+            plt.legend()
+            plt.show()
 
     def save_model(self, directory):
         self.model.save(directory)
@@ -161,4 +192,5 @@ if __name__ == '__main__':
 
     model = eostf( args.training, module=args.module, iterations=args.iterations )
     model.train_model()
+    model.display_results()
     model.save_model(args.directory)
