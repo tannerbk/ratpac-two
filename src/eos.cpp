2c1bb075423e282aa5904216b5a6d85358e4ad38
tannerbk <tannerbk@berkeley.edu>
2023-01-31 14:49:59 -0800

    improve version reporting

--- src/eos.cpp
+++ src/eos.cpp
@@ -34,6 +34,7 @@ public:
 
 int main(int argc, char** argv) {
   auto parser = new RAT::AnyParse(argc, argv);
+  std::cout << "Eos version: " << RAT::EOSVERSION << std::endl;
   auto eos = EOS::Eos( parser, argc, argv );
   eos.Begin();
   eos.Report();
